69293be89b85dd5c53ee171cde055107ed697625
Emmanuel <zacmac807@gmail.com>
2024-06-30 16:25:21 +0300

    complete task

--- server/app.py
+++ server/app.py
@@ -20,19 +20,41 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+
+    bakeries = Bakery.query.all()
+    bakeries_list = [bakery.to_dict() for bakery in bakeries]
+    return jsonify(bakeries_list)
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
 
-@app.route('/baked_goods/by_price')
+    bakery =Bakery.query.filter(Bakery.id ==id).first()
+    bakery_dict =bakery.to_dict()
+    response = make_response(bakery_dict, 200)
+
+    return response
+
+
+@app.route("/baked_goods/by_price")
 def baked_goods_by_price():
-    return ''
+
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [baked_good.to_dict() for baked_good in baked_goods]
+    return jsonify(baked_goods_list)
+
+
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if baked_good:
+        return jsonify(baked_good.to_dict())
+    else:
+        return jsonify({'error': 'No baked goods found'}), 404
 
-if __name__ == '__main__':
+
+
+if __name__ == '__main__': 
     app.run(port=5555, debug=True)
